30429c516ee5445b7c2ecb3ba660fd33ada2de14
RedKeforiginal <redkef@gmail.com>
2026-02-01 02:28:59 -0500

    Define Vulkan-Hpp dispatcher storage explicitly

--- src/vulkanDispatchStorage.cpp
+++ src/vulkanDispatchStorage.cpp
@@ -1,2 +1,7 @@
-#define VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE
 #include "vulkanConfig.h"
+
+#if defined(VULKAN_HPP_DISPATCH_LOADER_DYNAMIC) && !defined(VULKAN_HPP_NO_DEFAULT_DISPATCHER)
+namespace vk::detail {
+	DispatchLoaderDynamic defaultDispatchLoaderDynamic;
+}
+#endif
